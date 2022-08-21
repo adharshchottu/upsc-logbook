c3f8388199b68d3ec7e81bebdb67723a98f8f249
Augu <augustine.ashal@sowlab.com>
2022-08-21 22:53:41 +0530

    footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import { Box, Center } from "@chakra-ui/react"
+
+const Footer = () => {
+    return <Box className="rainbow">
+        <Center>#StickToThePlan</Center>
+    </Box>
+}
+
+export default Footer
