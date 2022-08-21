61373d6986162498ba3cace00d9bca352d1c86c7
Augu <augustine.ashal@sowlab.com>
2022-08-21 22:55:08 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home'
-import Write from './pages/Write'
 import Adddailylog from './pages/Adddailylog'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 import { Route, Routes } from 'react-router-dom'
 import Dailylog from './pages/Dailylog'
 import { FirebaseContextProvider } from './contexts/FirebaseContext'
+import Optional from './pages/Optional'
 
 function App() {
 
@@ -13,10 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/write" element={<Write />} />
         <Route path="/dailylog" element={<Dailylog />} />
+        <Route path="/optional" element={<Optional />} />
         <Route path="/adddailylog" element={<Adddailylog />} />
       </Routes>
+      <Footer />
     </FirebaseContextProvider>
   </>
 
