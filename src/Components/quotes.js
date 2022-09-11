7432146c670cdfbbb19285406b01d7c16f79fe34
Augu <augustine.ashal@sowlab.com>
2022-09-11 10:27:48 +0530

    quote updates

--- src/Components/quotes.js
+++ src/Components/quotes.js
@@ -68,6 +68,225 @@ export const quotes = [
     {
         "quote": "Religion is the masterpiece of the art of animal training, for it trains people as to how they shall think.",
         "quoter": "Arthur Schopenhauer"
+    },
+    {
+        "quote": "Rarest of all human quality is consistency.",
+        "quoter": "Jeremy Bentham"
+    },
+    {
+        "quote": "Do not fear to be eccentric in opinion, for every opinion now accepted was once eccentric.",
+        "quoter": "Bertrand Russell"
+    },
+    {
+        "quote": "If there were in the world today any large number of people who desired their own happiness more than they desired the unhappiness of others, we could have paradise in a few years.",
+        "quoter": "Bertrand Russell"
+    },
+    {
+        "quote": "Longing for love, the search for knowledge and unbearable pity for the suffering of mankind.",
+        "quoter": "Bertrand Russell"
+    },
+    {
+        "quote": "I measure the progress of a community by the degree of progress which women have achieved.",
+        "quoter": "B R Ambedkar"
+    },
+    {
+        "quote": "Life should be great rather than long.",
+        "quoter": "B R Ambedkar"
+    },
+    {
+        "quote": "An investment in knowledge always pays the best interest.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Either write worth reading or do something worth writing.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Early to bed and early to rise makes a man healthy, wealthy and wise.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "He that can have patience can have what he will.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "If everyone is thinking alike then no one is thinking.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Instead of cursing the darkness, light a candle.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "It is the first responsibility of every citizen to question authority.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Justice will not be served until those who are unaffected are as outraged as those who are.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Lost time is never found again.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "You may delay but time will not.",
+        "quoter": "Benjamin Franklin"
+    },
+    {
+        "quote": "Before you embark on a journey of revenge, dig two graves.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "Consideration for others is the basis of a good life and good society.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "When it is obvious that the goals cannot be reached, don’t adjust the goals, adjust the action steps.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "Give a bowl of rice to a man and you will feed him for a day. Teach him how to grow his own rice and you will save his life.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "Hold faithfulness and sincerity as first principles.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "If your plan is for one year, plant rice. If your plan is for ten years plant trees. If your plan is for one hundred years, educate children.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "It does not matter how slowly you go as long as you do not stop.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "Our greatest glory is not in never falling, but in rising every time we fall.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "The man who asks a question is a fool for a minute, the man who does not ask is a fool for life.",
+        "quoter": "Confucius"
+    },
+    {
+        "quote": "Anyone who has never made a mistake has never tried anything new.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Any fool can know. The point is to understand.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Blind belief in authority is the greatest enemy of truth.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Coincidence is God’s way of remaining anonymous.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Education is what remains after one has forgotten what one has learned in school.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Everything must be made as simple as possible. But not simpler.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Genius is 1% talent and 99% hard work.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Great spirits have always encountered violent opposition from mediocre minds.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Gravitation is not responsible for people falling in love.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "Imagination is more important than knowledge.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "If you can’t explain it to a six-year-old, you don’t understand it yourself.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "I speak to everyone in the same way, whether he is the garbage man or the president of the university.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "I have no special talents. I am only passionately curious.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "If a cluttered desk is a sign of a cluttered mind, of what, then, is an empty desk a sign?",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "It is not that I’m so smart. But I stay with the questions much longer.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "If people are good only because they fear punishment, and hope for reward, then we are a sorry lot indeed.",
+        "quoter": "Einstein"
+    },
+    {
+        "quote": "The only thing we have to fear is fear itself.",
+        "quoter": "Franklin Roosevelt"
+    },
+    {
+        "quote": "The test of our progress is not whether we add more to the abundance of those who have much; it is whether we provide enough for those who have little.",
+        "quoter": "Franklin Roosevelt"
+    },
+    {
+        "quote": "Believe nothing, no matter where you read it, or who said it, no matter if I have said it unless it agrees with your own reason and your own common sense.",
+        "quoter": "Gautam Buddha"
+    },
+    {
+        "quote": "Doubt everything. Find your own light.",
+        "quoter": "Gautam Buddha"
+    },
+    {
+        "quote": "Every morning we are born again. What we do today is what matters most.",
+        "quoter": "Gautam Buddha"
+    },
+    {
+        "quote": "However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?",
+        "quoter": "Gautam Buddha"
+    },
+    {
+        "quote": "Ninety-nine per cent of the failures come from people who have the habit of making excuses.",
+        "quoter": "George Washington"
+    },
+    {
+        "quote": "Few men have virtue to withstand the highest bidder.",
+        "quoter": "George Washington"
+    },
+    {
+        "quote": "Human happiness and moral duty are inseparably connected.",
+        "quoter": "George Washington"
+    },
+    {
+        "quote": "If the freedom of speech is taken away, then dumb and silent we may be led, like sheep to the slaughter.",
+        "quoter": "George Washington"
+    },
+    {
+        "quote": "Knowledge is in every country the surest basis of public happiness.",
+        "quoter": "George Washington"
+    },
+    {
+        "quote": "My mother was the most beautiful woman I ever saw. All I am I owe to my mother. I attribute my success in life to the moral, intellectual and physical education I received from her.",
+        "quoter": "George Washington"
     }
-
 ]
