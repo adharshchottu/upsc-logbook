34f9241ea160113c368aa547ad411871063a1db0
Augu <augustine.ashal@sowlab.com>
2022-09-23 23:38:35 +0530

    meta tags update

--- src/App.js
+++ src/App.js
@@ -7,11 +7,24 @@ import Dailylog from './pages/Dailylog'
 import { FirebaseContextProvider } from './contexts/FirebaseContext'
 import Optional from './pages/Optional'
 import Quotebar from './Components/Quotebar'
+import { Helmet } from 'react-helmet'
 
 function App() {
 
   return <>
     <FirebaseContextProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>#StickToThePlan</title>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="UPSC #StickToThePlan"
+        />
+        <link rel="apple-touch-icon" href="logo.png" />
+      </Helmet>
       <Navbar />
       <Quotebar />
       <Routes>
