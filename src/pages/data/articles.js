cf2daa6c9888660ebfae03c0e59e8c158a60cfc2
Augu <joseyaugustine18@gmail.com>
2023-01-11 22:09:43 +0530

    articles data

---
+++ src/pages/data/articles.js
@@ -0,0 +1,111 @@
+export const articles = [
+    {
+        "articleNo": "1",
+        "article": `Name and territory of the Union`,
+        "part": "1"
+    },
+    {
+        "articleNo": "1(1)",
+        "article": `India, that is Bharat, shall be a Union of States`,
+        "part": "1"
+    },
+    {
+        "articleNo": "1(2)",
+        "article": `It shall be a Federation consisting of territories specified in the First Schedule`,
+        "part": "1"
+    },
+    {
+        "articleNo": "1(3)",
+        "article": `The territory of India shall comprise- (a) the territories of the states; (b) the Union territories.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "2",
+        "article": `Admission or establishment of new States`,
+        "part": "1"
+    },
+    {
+        "articleNo": "2(1)",
+        "article": `Parliament may by law admit into the Union, or establish, new States on such terms and conditions as it thinks fit`,
+        "part": "1"
+    },
+    {
+        "articleNo": "2(2)",
+        "article": `No new State shall be formed or be received into the Union without the consent of the States concerned.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "2(3)",
+        "article": `A Law referred to in clause (1) shall not be deemed to be an amendment of the Constitution for the purpose of Article 368.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3",
+        "article": `Formation of new States and alteration of areas, boundares or names of existing States.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3(1)",
+        "article": `Parliament can make laws to create new states by separating territory from existing states, 
+        merging multiple states or parts of states, or by attaching new territory to existing states.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3(2)",
+        "article": `If a bill to change the boundaries of any state is introduced in the Lok Sabha, 
+        the President must get the views of the state's legislative assembly before giving his assent to the bill.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3(3)",
+        "article": `Parliament can also make laws to create new Union territories by separating territory from 
+        states or merging multiple Union territories or parts of states or Union territories, or attaching 
+        new territory to existing Union territories.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3(4)",
+        "article": `The laws made under clause 1 can also include additional rules that Parliament deems necessary.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "3(5)",
+        "article": `These laws will not be considered an amendment to the Constitution as per Article 368.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4",
+        "article": `Laws made under articles 2 and 3 to provide for the alteration of the areas, boundres or 
+        names of States to be reserved for consideration of the President and not to be questioned in any court`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4(1)",
+        "article": `The laws made under Article 2 and Article 3 will have the force of a Constitution law and will not be questioned in any court.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4(2)",
+        "article": `The laws made under Article 2 and Article 3 will be effective immediately after the President 
+        gives his assent to the bill, and the President can also make rules for their implementation.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4(3)",
+        "article": `the President can ask any state affected by a law made under Article 2 and Article 3 to 
+        express its views on the law, and he must consider those views before giving his assent to the bill.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4(4)",
+        "article": `If a law made under Article 2 and Article 3 affects the boundaries of a state, 
+        the President must refer the bill to the legislative assembly of the state for its views.`,
+        "part": "1"
+    },
+    {
+        "articleNo": "4(5)",
+        "article": `if the President is satisfied that any provision of a law made under Article 2 or Article 3 
+        needs modification, the President may by order make such modifications in that law.`,
+        "part": "1"
+    }
+]
