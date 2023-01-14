de5407543a4c90eeb2859ddb2688c78ab948e13b
Augu <joseyaugustine18@gmail.com>
2023-01-14 16:52:05 +0530

    filter articles

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,12 +1,20 @@
-import { Badge, Box, Grid, GridItem } from "@chakra-ui/react";
-import {articles} from "./data/articles";
+import { Badge, Box, Center, Checkbox, Grid, GridItem, Stack, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { articles } from "./data/articles";
 
 const Articles = () => {
+    const intialState = { articles: true, clauses: true, subclauses: true }
+    const [filter, setFilter] = useState(intialState)
+
+    const [data, setData] = useState(articles)
+
     const ArticleBox = (props) => {
         const marginLeft = props.articleNo.indexOf("(") == "-1" ? 0 : (props.articleNo.indexOf(")(") == "-1" ? 3 : 6);
-        const bgColor = props.articleNo.indexOf("(") == "-1" ? "whatsapp.500" : (props.articleNo.indexOf(")(") == "-1" ? "whatsapp.300" : "whatsapp.100"); ;
+        const bgColor = props.articleNo.indexOf("(") == "-1" ? "whatsapp.500" : (props.articleNo.indexOf(")(") == "-1" ? "whatsapp.300" : "whatsapp.100");
+        
         return <>
-            <Box maxW='container.xl' minW={[0,'container.xl']} borderWidth='1px' borderRadius='lg' overflow='hidden' my={4} marginLeft={marginLeft} bg={bgColor}>
+            <Box maxW='container.xl' minW={[0, 'container.xl']} borderWidth='1px' borderRadius='lg' overflow='hidden' my={4} marginLeft={marginLeft} bg={bgColor} >
 
                 <Box p='6'>
                     <Box display='flex' alignItems='baseline'>
@@ -51,13 +59,65 @@ const Articles = () => {
 
     }
 
+    useEffect(() => {
+        const filterData = (filter, data) => {
+            const filterArticles = data.filter((d) => {
+                return (d.articleNo.indexOf("(") == "-1") && filter.articles
+            })
+            const filterClauses = data.filter((d) => {
+                return (d.articleNo.indexOf("(") != "-1") && (d.articleNo.indexOf(")(") == "-1") && filter.clauses
+            })
+            const filterSubclauses = data.filter((d) => {
+                return (d.articleNo.indexOf(")(") != "-1") && filter.subclauses
+            })
+            const unSorted = [...filterArticles, ...filterClauses, ...filterSubclauses]
+            const sorted = unSorted.sort(function (a, b) {
+                let aNum = parseInt(a.articleNo.match(/\d+/)[0]);
+                let bNum = parseInt(b.articleNo.match(/\d+/)[0]);
+                if (aNum === bNum) {
+                    return a.articleNo.localeCompare(b.articleNo);
+                }
+                return aNum - bNum;
+            });
+            return sorted;
+        }
+        const filteredData = filterData(filter, articles)
+        setData(filteredData)
+    })
+
     return <>
+        <Center my={"4"}>
+            <Text
+                bgGradient='linear(to-l, #7928CA, whatsapp.500)'
+                bgClip='text'
+                fontSize='6xl'
+                fontWeight='extrabold'
+            >
+                Articles
+            </Text>
+        </Center>
+        <Center>
+            <Stack spacing={[1, 5]} direction={['column', 'row']} mx={"2"}>
+                <Checkbox size='lg' colorScheme='linkedin' defaultChecked
+                    onChange={(e) => setFilter({ articles: !filter.articles, clauses: filter.clauses, subclauses: filter.subclauses })}>
+                    Articles
+                </Checkbox>
+                <Checkbox size='lg' colorScheme='linkedin' defaultChecked
+                    onChange={(e) => setFilter({ articles: filter.articles, clauses: !filter.clauses, subclauses: filter.subclauses })}>
+                    Clauses
+                </Checkbox>
+                <Checkbox size='lg' colorScheme='linkedin' defaultChecked
+                    onChange={(e) => setFilter({ articles: filter.articles, clauses: filter.clauses, subclauses: !filter.subclauses })}>
+                    Sub-clauses
+                </Checkbox>
+            </Stack>
+        </Center>
         <Grid
             templateColumns='10px 30px 1fr'
             m={"2"}
         >
             {
-                articles.map(d => <ArticleItem data={d} key={d.articleNo}/>)
+                data.map(d => <ArticleItem data={d} key={d.articleNo} />)
             }
         </Grid>
     </>
