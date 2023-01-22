c02dde88196ebcb83407110e2dcec293101d784a
Augu <joseyaugustine18@gmail.com>
2023-01-22 16:07:17 +0530

    history data

---
+++ src/pages/data/history.js
@@ -0,0 +1,6 @@
+export const history = [
+    {
+        "year":72,
+        "history":"generally accepted date of Kanishka's accession and the beginning of the Shaka era"
+    }
+]
---
+++ src/pages/data/history_bc.js
@@ -0,0 +1,100 @@
+export const historyBC = [
+    {
+        "year": 2500,
+        "history": "2500-1700 B.C.",
+        "notes": "The Harappa culture"
+    },
+    {
+        "year": 1500,
+        "history": "The arrival of Aryans to the Indian subcontinent",
+        "notes": "They came from North-eastern Iran and the region around the Caspian sea"
+    },
+    {
+        "year": 1200,
+        "history": "1200-800 and later",
+        "notes": "eastward expansion of Aryans into the Ganga valley was made easier by the discovery of the use of iron"
+    },
+    {
+        "year": 600,
+        "history": "The rise of the sixteen mahajanapadas of North India",
+        "notes": "the sub notes"
+    },
+    {
+        "year": 599,
+        "history": "599-527 B.C.",
+        "notes": "Mahavira was born in the city of Vaishali and he taught Jainism"
+    },
+    {
+        "year": 563,
+        "history": "563-483 B.C.",
+        "notes": "Siddhartha was born in the Lumbini grove near the city of Kapilavastu and he taught Buddhism"
+    },
+    {
+        "year": 542,
+        "history": "Bimbisara became the king of Magadha",
+    },
+    {
+        "year": 492,
+        "history": "Ajatashatru became the king of Magadha",
+    },
+    {
+        "year": 400,
+        "history": "4th century",
+        "notes": "Nandas rule in Magadha"
+    },
+    {
+        "year": 327,
+        "history": "Alexander's invasion",
+        "notes": "Greek king Alexander conquered Punjab and after the death of Alexander, Punjab was ruled by Greek governors."
+    },
+    {
+        "year": 321,
+        "history": "Chandragupta Maurya establishes the Mauryan Empire by overthrowing the Nanda kings",
+        "notes": "Chanakya also known as Kautilya trained Chandragupta."
+    },
+    {
+        "year": 303,
+        "history": "Seleucus Nicator defeated by Chandragupta Maurya",
+    },
+    {
+        "year": 269,
+        "history": "269-232 Ashoka",
+        "notes": "Chandragupta => Bindusara => Ashoka"
+    },
+    {
+        "year": 260,
+        "history": "Ashoka fought a war against Kalinga and was victorious.",
+        "notes": "After the Kalinga war, Ashoka decided not to fight any more wars."
+    },
+    {
+        "year": 185,
+        "history": "Fall of the Mauryan empire",
+    },
+    {
+        "year": 100,
+        "history": "Bactrian rule in the North-West",
+    },
+    {
+        "year": 100,
+        "history": "Rise of the Satavahanas",
+        "notes": "With the decline of the Mauryan empire, the Satavahanas emerged as an independent power in the Deccan."
+    },
+    {
+        "year": 100,
+        "history": "Megalithic culture of south India",
+    },
+    {
+        "year": 58,
+        "history": "Beginnning the vikram era",
+        "notes": "A commonly used system of dating in ancient India"
+    },
+    {
+        "year": 50,
+        "history": "Shakas in western India",
+        "notes": "The Satavahans had to fight against the shakas who were ruling in Saurashtra."
+    },
+    {
+        "year": 1,
+        "history": "The shanka period in the south",
+    }
+]
