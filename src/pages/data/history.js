498dcc8f37f34c7d8c01a9d78a6a76773cff0ed4
Augu <joseyaugustine18@gmail.com>
2023-01-22 22:09:36 +0530

    update in AD history

--- src/pages/data/history.js
+++ src/pages/data/history.js
@@ -1,6 +1,74 @@
 export const history = [
     {
         "year":72,
-        "history":"generally accepted date of Kanishka's accession and the beginning of the Shaka era"
-    }
+        "history":"generally accepted date of Kanishka's accession and the beginning of the Shaka era",
+    },
+    {
+        "year":200,
+        "history":"decline of the Kushanas",
+    },
+    {
+        "year":200,
+        "history":"decline of the Satavahanas",
+    },
+    {
+        "year":320,
+        "history":"Chandragupta I",
+        "notes": "beginning of the Gupta era"
+    },
+    {
+        "year":330,
+        "history":"Samudragupta",
+    },
+    {
+        "year":380,
+        "history":"Chandragupta II",
+        "notes": "also known as Vikramaditya"
+    },
+    {
+        "year":405,
+        "history":"Fa Hien in India",
+        "notes": "a Chinese traveller who came to India to study Buddhist scriptures"
+    },
+    {
+        "year":450,
+        "history":"The first Huna attack on the Gupta empire",
+    },
+    {
+        "year":500,
+        "history":"The pallavas establish their kingdom",
+    },
+    {
+        "year":500,
+        "history":"The rise of the chalukyas of vatapi",
+    },
+    {
+        "year":569,
+        "history":"Islam arose in Arabia and soon spread to many parts of Asia and Africa. ",
+        "notes": "It was propagated by the Prophet Muhammad who saw it as a means of unifying the tribes of Arabia."
+    },
+    {
+        "year":606,
+        "history":"Harshavardhana",
+    },
+    {
+        "year":630,
+        "history":"Hieun Tsang in India",
+    },
+    {
+        "year":608,
+        "history":"pulakeshin, the chalukya king",
+    },
+    {
+        "year":600,
+        "history":"Tamil saints - alvar and Nayanmars",
+    },
+    {
+        "year":622,
+        "history":"The beginning of the Hijra era",
+    },
+    {
+        "year":712,
+        "history":"The arab conquest of Sind",
+    },
 ]
