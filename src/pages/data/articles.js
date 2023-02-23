9f45bccb5972bfe982d2dbee1922e3a4a29ed079
Augu <joseyaugustine18@gmail.com>
2023-02-24 01:36:04 +0530

    added DPSP in the articles

--- src/pages/data/articles.js
+++ src/pages/data/articles.js
@@ -257,5 +257,194 @@ export const articles = [
         "articleNo": "35",
         "article": `Legislation to give effect to the provisions of this Part.`,
         "part": "III"
+    },
+    {
+        "articleNo": "36",
+        "article": `In this Part, unless the context otherwise requires, “the State” has the same meaning as in Part III.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "37",
+        "article": `The provisions contained in this Part shall not be enforceable by any court, but the principles therein laid
+        down are nevertheless fundamental in the governance of the country and it shall be the duty of the State to apply
+        these principles in making laws`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "38",
+        "article": `State to secure a social order for the promotion of welfare of the people.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "38(1)",
+        "article": `The State shall strive to promote the welfare of the people by securing and protecting as effectively as
+        it may a social order in which justice, social, economic and political, shall inform all the institutions of the
+        national life.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "38(2)",
+        "article": `The State shall, in particular, strive to minimise the inequalities in income, and endeavour to eliminate
+        inequalities in status, facilities and opportunities, not only amongst individuals but also amongst groups of
+        people residing in different areas or engaged in different vocations.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39",
+        "article": `Certain principles of policy to be followed by the State.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(a)",
+        "article": `The State shall, in particular, direct its policy towards securing that the citizens, men and women equally, have
+        the right to an adequate means of livelihood`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(b)",
+        "article": `The State shall, in particular, direct its policy towards securing that the ownership and control of the material
+        resources of the community are so distributed as best to subserve the common good`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(c)",
+        "article": `The State shall, in particular, direct its policy towards securing that the operation of the economic system does
+        not result in the concentration of wealth and means of production to the common detriment`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(d)",
+        "article": `The State shall, in particular, direct its policy towards securing that there is equal pay for equal work for both
+        men and women`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(e)",
+        "article": `The State shall, in particular, direct its policy towards securing that the health and strength of workers, men
+        and women, and the tender age of children are not abused and that citizens are not forced by economic
+        necessity to enter avocations unsuited to their age or strength`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(1)(f)",
+        "article": `The State shall, in particular, direct its policy towards securing that children are given opportunities and
+        facilities to develop in a healthy manner and in conditions of freedom and dignity and that childhood
+        and youth are protected against exploitation and against moral and material abandonment`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "39(a)",
+        "article": `The State shall secure that the operation of the legal system promotes justice, on a basis of equal 
+        opportunity, and shall, in particular, provide free legal aid, by suitable legislation or schemes or in any other way, 
+        to ensure that opportunities for securing justice are not denied to any citizen by reason of economic or other disabilities.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "40",
+        "article": `The State shall take steps to organise village panchayats and endow them with such powers and 
+        authority as may be necessary to enable them to function as units of self-government.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "41",
+        "article": `The State shall, within the limits of its economic capacity and development, make effective provision 
+        for securing the right to work, to education and to public assistance in cases of unemployment, old age, sickness 
+        and disablement, and in other cases of undeserved want.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "42",
+        "article": `The State shall make provision for securing just and humane conditions of work and for maternity relief`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "43",
+        "article": `The State shall make provision for securing just and humane conditions of work and for maternity 
+        relief standard of life and full enjoyment of leisure and social and cultural opportunities and, in particular, 
+        the State shall endeavour to promote cottage industries on an individual or co-operative basis in rural areas.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "43(a)",
+        "article": `he State shall take steps, by suitable legislation or in any other way, to secure the participation 
+        of workers in the management of undertakings, establishments or other organisations engaged in any industry`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "44",
+        "article": `The State shall endeavour to secure for the citizens a uniform civil code throughout the territory of India.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "45",
+        "article": `Provision for free and compulsory education for children`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "46",
+        "article": `The State shall promote with special care the educational and economic interests of the weaker 
+        sections of the people, and, in particular, of the Scheduled Castes and the Scheduled Tribes, and shall 
+        protect them from social injustice and all forms of exploitation`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "47",
+        "article": `The State shall regard the raising of the level of nutrition and the standard of 
+        living of its people and the improvement of public health as among its primary duties and, 
+        in particular, the State shall endeavour to bring about prohibition of the consumption except 
+        for medicinal purposes of intoxicating drinks and of drugs which are injurious to health.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "48",
+        "article": `The State shall endeavour to organise agriculture and animal husbandry on modern and scientific 
+        lines and shall, in particular, take steps for preserving and improving the breeds, and prohibiting the 
+        slaughter, of cows and calves and other milch and draught cattle`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "48(A)",
+        "article": `The State shall endeavour to protect and improve the environment and to safeguard the forests
+        and wild life of the country.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "49",
+        "article": ` It shall be the obligation of the State to protect every monument or place or object of 
+        artistic or historic interest, declared by or under law made by Parliament to be of national importance, 
+        from spoliation, disfigurement, destruction, removal, disposal or export, as the case may be`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "50",
+        "article": `The State shall take steps to separate the judiciary from the executive in the public services of the State`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "51",
+        "article": `Promotion of international peace and security.`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "51(A)",
+        "article": `The State shall endeavour to promote international peace and security`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "51(B)",
+        "article": `The State shall endeavour to maintain just and honourable relations between nations`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "51(C)",
+        "article": `The State shall endeavour to foster respect for international law and treaty
+        obligations in the dealings of organized peoples with one another`,
+        "part": "IV"
+    },
+    {
+        "articleNo": "51(D)",
+        "article": `The State shall endeavour to encourage settlement of international disputes
+        by arbitration.`,
+        "part": "IV"
     }
 ]
